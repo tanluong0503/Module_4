b4bc749a25566026602180ebe4454d9091280391
Your Name <you@example.com>
2022-11-14 09:08:18 +0700

    module_4

---
+++ json-server/assets/js/app.js
@@ -0,0 +1,11 @@
+class Customer {
+    constructor(id, fullName, email, phone, address, balance, deleted) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.balance = balance;
+        this.deleted = deleted;
+    }
+}
